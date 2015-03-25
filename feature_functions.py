70cb43234134e463384a2d9e40f1846733eaab96
lordwarlock <zheng@ZhihaoZhengdeMacBook-Pro.local>
2015-03-25 14:13:46 -0400

    add link word features and result of svmtree

--- feature_functions.py
+++ feature_functions.py
@@ -2,7 +2,10 @@ import re
 import inflect
 
 def get_feature_functions():
-    return [dist_0,dist_1,dist_4,dist_6,entity_type]
+    return [dist_0,dist_1,dist_4,dist_6,entity_type,
+            hint_comma_detection,hint_of_detection,hint_and_detection,
+            hint_s_detection,hint_the_detection,hint_from_detection,hint_in_detection,
+            hint_at_detection,hint_like_detection,hint_for_detection]
 
 def distance_n(coref,n):
     """number of word between two mentions"""
@@ -32,16 +35,44 @@ def hint_word_detection(coref,corpus,token_distance = 3,hint_words = ['is','are'
     sentence = corpus.postagged_data[document][coref.first.sent].tokens
     for i in range(coref.first.start,coref.second.end):
         if (sentence[i][0] in hint_words):
-            print coref.first.word,coref.first.end, coref.second.word,coref.second.start
-            print sentence[coref.first.start:coref.second.end]
+            #print coref.first.word,coref.first.end, coref.second.word,coref.second.start
+            #print sentence[coref.first.start:coref.second.end]
             return True
 
     return False
+
 def hint_comma_detection(coref,corpus):
     return hint_word_detection(coref,corpus,hint_words = [','],token_distance=6)
 
+def hint_of_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['of'],token_distance=6)
+
+def hint_and_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['and'],token_distance=6)
+
+def hint_s_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ["'s","'"],token_distance=6)
+
+def hint_the_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['the'],token_distance=6)
+
+def hint_from_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['from'],token_distance=6)
+
+def hint_in_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['in'],token_distance=6)
+
+def hint_at_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['at'],token_distance=6)
+
+def hint_like_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['like'],token_distance=6)
+
+def hint_for_detection(coref,corpus):
+    return hint_word_detection(coref,corpus,hint_words = ['for'],token_distance=6)
+
 def hint_who_detection(coref,corpus):
-    return hint_word_detection(coref,corpus,hint_words = ['who','which'],token_distance=5)
+    return hint_word_detection(coref,corpus,hint_words = ['who','which'],token_distance=6)
 
 if __name__ == '__main__':
     from data_reader import *
--- translate_class.py
+++ translate_class.py
@@ -1,6 +1,9 @@
 import re
+from subprocess import call
+
 def translate_class(in_file = './project3/data/svm-light-files/rel-train-parsed-data',
-                    out_file = './project3/data/svm-ova-light-files/rel-train-parsed-data'):
+                    out_file = './project3/data/svm-ova-light-files/rel-train-parsed-data',
+                    tag = 'EMP-ORG.Employ-Staff.reverse'):
     f_out = open(out_file,'w')
     f_in = open(in_file,'r')
     counter = 0
@@ -8,24 +11,34 @@ def translate_class(in_file = './project3/data/svm-light-files/rel-train-parsed-
         match = re.match('(.*? )(\|BT\|.*)',line)
         #if match and match.group(1).strip() == 'PHYS.Located':
         #PER-SOC 88/50 EMP-ORG 81/50
-        if match and 'EMP-ORG.Employ-Staff.reverse' in match.group(1).strip():
+        if match and tag in match.group(1).strip():
             f_out.write('1' + ' ' + match.group(2)+'\n')
         else:
             f_out.write('-1' + ' ' + match.group(2)+'\n')
         counter += 1
-<<<<<<< HEAD
-=======
+
         #if (counter == 600): break
->>>>>>> origin/master
+
     f_out.close()
     f_in.close()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    for data in ['train','dev','test']:
-        translate_class('./project3/data/svm-light-files/rel-{}-parsed-data'.format(data),'./project3/data/svm-ova-light-files/rel-{}-parsed-data'.format(data))
-=======
-    translate_class()
-    translate_class('./project3/data/svm-light-files/rel-test-parsed-data',
-                    './project3/data/svm-ova-light-files/rel-test-parsed-data')
->>>>>>> origin/master
+    with open('./stats/train_classes','r') as f_c:
+        for line in f_c:
+            splitted = line.split()
+            tag = splitted[0]
+            print tag
+            o_file = '/Users/zheng/documents/ie/ie_proj3/project3/data/svm-ova-light-files/results-1/log-{}'.format(tag)
+            f_o = open(o_file,'w')
+            for data in ['train','dev','test']:
+                translate_class('./project3/data/svm-light-files/rel-{}-parsed-data'.format(data),'./project3/data/svm-ova-light-files/rel-{}-parsed-data'.format(data),tag)
+            call(['/Users/zheng/documents/tools/svm-light-TK-1.2/svm-light-TK-1.2.1/svm_learn',
+                  '-t','5','-D','0',
+                  '/Users/zheng/documents/ie/ie_proj3/project3/data/svm-ova-light-files/rel-train-parsed-data',
+                  '/Users/zheng/documents/ie/ie_proj3/project3/data/svm-ova-light-files/train_model'])
+
+            call(['/Users/zheng/documents/tools/svm-light-TK-1.2/svm-light-TK-1.2.1/svm_classify',
+                  '/Users/zheng/documents/ie/ie_proj3/project3/data/svm-ova-light-files/rel-test-parsed-data',
+                  '/Users/zheng/documents/ie/ie_proj3/project3/data/svm-ova-light-files/train_model',
+                  '/Users/zheng/documents/ie/ie_proj3/project3/data/svm-ova-light-files/results-1/{}'.format(tag)],
+                  stdout=f_o)
